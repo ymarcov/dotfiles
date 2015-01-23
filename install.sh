86c9ffab8d70f5fba25093629b1ec5973a774590
Yam Marcovic <ymarcov@gmail.com>
2015-01-23 23:38:09 +0200

    Add installation script

---
+++ install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+mkdir -p ~/.i3
+
+echo "Backing up old files..."
+mv -f ~/.vimrc{.old}
+mv -f ~/.i3/config{.old}
+
+echo "Creating links..."
+ln -sv $PWD/vimrc ~/.vimrc
+ln -sv $PWD/i3.config ~/.i3/config
